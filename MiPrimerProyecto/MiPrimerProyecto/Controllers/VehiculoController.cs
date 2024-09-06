4eeda11b226fe65402733032dc1eac9c89ecc8c1
ebarrientos1979 <edwin.barrientos@iteraprocess.com>
2024-09-05 19:22:57 -0500

    MiPrimerProyecto
    MiPrimerProyecto

---
+++ MiPrimerProyecto/MiPrimerProyecto/Controllers/VehiculoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiPrimerProyecto.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class VehiculoController:ControllerBase
+    {
+        //TOKEN
+        [HttpGet("{nombre}")]
+        public IActionResult Get(string nombre)
+        {
+            string token = "dgfsdgdfgdfgd3425234gefgdf";
+            return Ok(new
+            {
+                token = token,
+                mensaje = ""
+
+            });
+            
+        }
+    }
+}
